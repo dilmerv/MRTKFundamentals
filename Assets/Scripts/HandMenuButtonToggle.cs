f55854f13a313abdf61da625e84202be5722f1d1
Dilmer Valecillos <dilmervst@gmail.com>
2021-05-07 17:14:10 -0600

    Added new hand menu example

---
+++ Assets/Scripts/HandMenuButtonToggle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class HandMenuButtonToggle : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject targetObject;
+
+    private bool isButtonActive = false;
+
+    public void Toggle()
+    {
+        isButtonActive = !isButtonActive;
+
+        if (targetObject != null)
+        {
+            targetObject.SetActive(isButtonActive);
+        }
+    }
+}
