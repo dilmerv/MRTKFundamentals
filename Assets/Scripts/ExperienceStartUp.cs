83fbf94d8585437fe7e13d7af3f9f36b6ebd4e9f
Dilmer Valecillos <dilmervst@gmail.com>
2021-05-11 20:42:48 -0600

    Hiding hand menu on start

--- Assets/Scripts/ExperienceStartUp.cs
+++ Assets/Scripts/ExperienceStartUp.cs
@@ -5,10 +5,20 @@ public class ExperienceStartUp : MonoBehaviour
     [SerializeField]
     private Transform environmentTransform;
 
+    [SerializeField]
+    private Transform handTransforms;
+
     void Awake()
     {
+        HideAll(environmentTransform);
+
+        HideAll(handTransforms);
+    }
+
+    private void HideAll(Transform transformToHide)
+    {
         // make sure to hide all environment components by default
-        foreach (Transform transform in environmentTransform)
+        foreach (Transform transform in transformToHide)
         {
             transform.gameObject.SetActive(false);
         }
