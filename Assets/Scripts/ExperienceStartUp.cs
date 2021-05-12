66c0a5a4a2c8fe068b5f302faaf154d6cb08887f
Dilmer Valecillos <dilmervst@gmail.com>
2021-05-11 20:11:15 -0600

    Added experience setup script to hide all environment components

---
+++ Assets/Scripts/ExperienceStartUp.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ExperienceStartUp : MonoBehaviour
+{
+    [SerializeField]
+    private Transform environmentTransform;
+
+    void Awake()
+    {
+        // make sure to hide all environment components by default
+        foreach (Transform transform in environmentTransform)
+        {
+            transform.gameObject.SetActive(false);
+        }
+    }
+}
